a1852189e44d821aafa49036dc68ce3a6fdcca21
Rodrigo Leite <rodrigoleitetr@gmail.com>
2019-10-31 23:19:40 -0300

    Adding part 1 of the tutorial

--- internet-banking-app/src/app/app.module.ts
+++ internet-banking-app/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
